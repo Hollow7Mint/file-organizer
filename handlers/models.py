f159a4e82290dc159afa5b53e09e86eac2e83909
James Holloway <james.holloway94@gmail.com>
2026-04-30 14:39:30 +0000

    resolve known_hosts cleanup sync into metadata

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-04-30 13:46:00 UTC
+# Last sync: 2026-04-30 14:39:30 UTC
