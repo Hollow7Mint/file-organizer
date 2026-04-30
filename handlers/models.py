a583c579e916e45b4c429447cac940fcff25d6ff
James Holloway <james.holloway94@gmail.com>
2026-04-30 05:27:53 +0000

    bump s3 bucket policy sync after review

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-04-30 04:45:05 UTC
+# Last sync: 2026-04-30 05:27:53 UTC
