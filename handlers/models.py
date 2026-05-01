45fed12532574b13e8347600d26181685c48f0e8
James Holloway <james.holloway94@gmail.com>
2026-05-01 07:37:45 +0000

    touch redis sentinel sync during scheduled refresh

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-01 06:40:18 UTC
+# Last sync: 2026-05-01 07:37:45 UTC
