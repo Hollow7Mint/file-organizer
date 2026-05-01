a49eccf52fbb9f4b8b47f81e2f47852a2865d6ef
James Holloway <james.holloway94@gmail.com>
2026-05-01 10:23:20 +0000

    preserve nat gateway sync time in infra banner

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-01 09:51:06 UTC
+# Last sync: 2026-05-01 10:23:20 UTC
