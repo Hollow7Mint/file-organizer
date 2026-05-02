ca9fc41e996a8a43230a2c26793ae4fd48d28d0c
James Holloway <james.holloway94@gmail.com>
2026-05-02 03:39:15 +0000

    merge cloudformation stack apply time into record

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-02 02:56:27 UTC
+# Last sync: 2026-05-02 03:39:15 UTC
