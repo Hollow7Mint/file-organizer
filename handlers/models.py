f981f7fdf175fe2ab957167a9b0e94b0bc33eb26
James Holloway <james.holloway94@gmail.com>
2026-05-02 06:01:32 +0000

    publish ssh key distribution sync after rotation

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-02 04:09:36 UTC
+# Last sync: 2026-05-02 06:01:31 UTC
