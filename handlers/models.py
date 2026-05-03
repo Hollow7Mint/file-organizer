a88d30ca131b4a4cf10e71db627f2adee4e14c17
James Holloway <james.holloway94@gmail.com>
2026-05-03 00:52:57 +0000

    bridge ssh ProxyJump config sync through banner

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-02 23:19:19 UTC
+# Last sync: 2026-05-03 00:52:57 UTC
