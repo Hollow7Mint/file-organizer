6ce5df23c0a337539ab4f19ed6bb5135a169ce9c
James Holloway <james.holloway94@gmail.com>
2026-05-06 02:13:32 +0000

    bump known_hosts update stamp after rotation

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-06 01:37:01 UTC
+# Last sync: 2026-05-06 02:13:32 UTC
