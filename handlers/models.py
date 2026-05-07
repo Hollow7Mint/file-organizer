0a2a2de537bd2d0861d76acf63c19f7d03b33f74
James Holloway <james.holloway94@gmail.com>
2026-05-07 22:21:20 +0000

    anchor ssh client config sync after key rotation

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-07 21:21:31 UTC
+# Last sync: 2026-05-07 22:21:20 UTC
