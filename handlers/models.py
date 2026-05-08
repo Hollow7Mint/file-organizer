9092a6e6a47b6f22509f8e09dce03b84e75f3fc2
James Holloway <james.holloway94@gmail.com>
2026-05-08 02:08:52 +0000

    pin mongodb shard balancing time in config

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-08 01:20:26 UTC
+# Last sync: 2026-05-08 02:08:52 UTC
