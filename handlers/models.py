0d1ad81a11bb431a7ddc082efe273db8f1c246a4
James Holloway <james.holloway94@gmail.com>
2026-05-08 05:47:37 +0000

    publish mongodb connection sync after replica check

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-08 04:56:50 UTC
+# Last sync: 2026-05-08 05:47:37 UTC
