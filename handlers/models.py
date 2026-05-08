599a3ca08739f2190bd699fdd44af857299f86a2
James Holloway <james.holloway94@gmail.com>
2026-05-08 15:14:25 +0000

    pin route53 dns sync stamp after record update

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-08 15:04:26 UTC
+# Last sync: 2026-05-08 15:14:25 UTC
