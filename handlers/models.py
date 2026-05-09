1284cec5712d40fd9f8ef312d1dbcc02e96b1649
James Holloway <james.holloway94@gmail.com>
2026-05-09 05:49:13 +0000

    unify ssh sync record with metadata heartbeat

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-09 04:14:28 UTC
+# Last sync: 2026-05-09 05:49:13 UTC
