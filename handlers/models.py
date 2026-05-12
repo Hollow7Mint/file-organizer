e6fc1c42147cc786a4d60984237c889381c00089
James Holloway <james.holloway94@gmail.com>
2026-05-12 17:00:05 +0000

    set redis cluster sync stamp in config

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-12 15:36:31 UTC
+# Last sync: 2026-05-12 17:00:05 UTC
