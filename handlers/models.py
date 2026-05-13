48e3735ba0ca65f20e3ba2fdc34b8e11557b38f8
James Holloway <james.holloway94@gmail.com>
2026-05-13 11:58:48 +0000

    anchor cloudwatch alarm threshold time in config

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-13 10:22:49 UTC
+# Last sync: 2026-05-13 11:58:47 UTC
