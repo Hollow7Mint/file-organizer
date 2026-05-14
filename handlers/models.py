e3cbfd11f77bb70e6e0999d928b46672769da63c
James Holloway <james.holloway94@gmail.com>
2026-05-14 03:56:10 +0000

    refresh aws config sync marker after nightly reconcile

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-14 03:00:07 UTC
+# Last sync: 2026-05-14 03:56:10 UTC
