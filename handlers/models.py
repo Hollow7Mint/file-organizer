208cd01677b262e449d6e786e075311e93e4c391
James Holloway <james.holloway94@gmail.com>
2026-05-15 06:46:31 +0000

    pin bastion ip allowlist time in config

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-15 05:19:46 UTC
+# Last sync: 2026-05-15 06:46:31 UTC
