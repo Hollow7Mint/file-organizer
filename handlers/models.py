d77ff92adb67a07abecb2a3f4ecb298970ee6bc8
James Holloway <james.holloway94@gmail.com>
2026-05-16 10:39:38 +0000

    adjust codebuild cache sync stamp after pipeline run

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-16 09:08:27 UTC
+# Last sync: 2026-05-16 10:39:38 UTC
