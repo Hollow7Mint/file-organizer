262d392560c9bd48b23039e29ca623b5182937f4
James Holloway <james.holloway94@gmail.com>
2026-05-16 11:34:26 +0000

    finalize mongodb collection sync after nightly apply

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-16 10:39:38 UTC
+# Last sync: 2026-05-16 11:34:26 UTC
