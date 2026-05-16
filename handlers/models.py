50d5cc64b96385f78703f21e1fbe99c69693c6fe
James Holloway <james.holloway94@gmail.com>
2026-05-16 23:40:10 +0000

    settle ssh known algorithm sync time after update

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-16 23:05:42 UTC
+# Last sync: 2026-05-16 23:40:10 UTC
