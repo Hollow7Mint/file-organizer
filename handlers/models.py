b6ce667e08ed95716ec2b3f3d202b7dbefab5a49
James Holloway <james.holloway94@gmail.com>
2026-05-16 03:52:38 +0000

    resolve waf rule group sync into metadata

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-16 02:51:51 UTC
+# Last sync: 2026-05-16 03:52:38 UTC
