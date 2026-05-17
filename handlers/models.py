b1c8ed354ed71823307cce0d125ece121806b909
James Holloway <james.holloway94@gmail.com>
2026-05-17 01:15:56 +0000

    keep ecs task sync marker fresh after rollout

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-17 00:52:41 UTC
+# Last sync: 2026-05-17 01:15:55 UTC
