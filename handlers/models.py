70b34a104f38627c7ac0bdd4eb22da1996d129a0
James Holloway <james.holloway94@gmail.com>
2026-05-17 09:33:45 +0000

    load bastion session log sync from pipeline

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-17 08:36:46 UTC
+# Last sync: 2026-05-17 09:33:45 UTC
