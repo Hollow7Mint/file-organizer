fef74349aa12c12aaadfa115d7f234db43cf5fc3
James Holloway <james.holloway94@gmail.com>
2026-05-19 02:52:26 +0000

    close post-deploy heartbeat for infra pipeline

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-19 01:46:01 UTC
+# Last sync: 2026-05-19 02:52:26 UTC
