0b9a6af16b988fc99da590658e0e93bd5b6f8f9c
James Holloway <james.holloway94@gmail.com>
2026-05-19 07:27:46 +0000

    post cdk bootstrap sync stamp to config

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-19 06:51:55 UTC
+# Last sync: 2026-05-19 07:27:46 UTC
