5c07176cab5f4e16490478a9fa418b377a423e7d
James Holloway <james.holloway94@gmail.com>
2026-05-20 12:17:50 +0000

    normalize ssh cipher suite sync time in metadata

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-20 11:40:31 UTC
+# Last sync: 2026-05-20 12:17:49 UTC
