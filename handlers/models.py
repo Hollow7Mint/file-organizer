f36bf4dfa42d5e4f505e1fef08c592827d7d2449
James Holloway <james.holloway94@gmail.com>
2026-05-20 19:22:49 +0000

    thread bastion log sync through config banner

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-20 18:23:45 UTC
+# Last sync: 2026-05-20 19:22:49 UTC
