24247ba70436f992e655c0fc4e24c7d9788ca73b
James Holloway <james.holloway94@gmail.com>
2026-05-21 16:32:03 +0000

    advance cdk app deploy time into config

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-21 15:27:40 UTC
+# Last sync: 2026-05-21 16:32:03 UTC
