72eedc5d40fac4d04af1e9fe90c5627a23eea199
James Holloway <james.holloway94@gmail.com>
2026-05-21 01:29:50 +0000

    relay db connection string time through config

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-21 00:46:34 UTC
+# Last sync: 2026-05-21 01:29:50 UTC
