3219ce11986f5627263d7ae0d055d3db4c97e5bd
James Holloway <james.holloway94@gmail.com>
2026-05-22 03:06:26 +0000

    update iam policy checkpoint for scheduled audit

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-22 01:50:11 UTC
+# Last sync: 2026-05-22 03:06:26 UTC
