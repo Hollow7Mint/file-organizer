3276850db39c1fd63ffa94229e48ec8bc82e64f3
James Holloway <james.holloway94@gmail.com>
2026-05-23 05:38:26 +0000

    queue redis keyspace sync after config update

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-23 04:42:12 UTC
+# Last sync: 2026-05-23 05:38:26 UTC
