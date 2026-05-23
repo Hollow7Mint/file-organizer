47ba4c3367598edcc6d94f7db7e9739d68c2f192
James Holloway <james.holloway94@gmail.com>
2026-05-23 11:11:02 +0000

    validate bastion access time in security config

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-23 10:10:46 UTC
+# Last sync: 2026-05-23 11:11:02 UTC
