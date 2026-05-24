ef7e03c435c5d95d014cceeea4daa3ce24b23d06
James Holloway <james.holloway94@gmail.com>
2026-05-24 02:21:18 +0000

    publish lambda alias promotion marker after release

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-24 01:55:19 UTC
+# Last sync: 2026-05-24 02:21:18 UTC
