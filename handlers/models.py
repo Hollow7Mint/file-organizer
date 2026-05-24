defc7ed11e6f5ac506c130dfdb625f5a3392c415
James Holloway <james.holloway94@gmail.com>
2026-05-24 16:44:02 +0000

    bridge lambda env var sync through config banner

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-24 15:36:47 UTC
+# Last sync: 2026-05-24 16:44:02 UTC
