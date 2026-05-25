6d8abd1cddd4e54793bb77ae280d53ca0e36426b
James Holloway <james.holloway94@gmail.com>
2026-05-25 22:36:54 +0000

    finalize vpc security group sync after rule push

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-25 22:06:02 UTC
+# Last sync: 2026-05-25 22:36:54 UTC
