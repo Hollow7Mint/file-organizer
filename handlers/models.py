b8c2d67e3ac6da23ce79ef4edb97fc22ef76d0ec
James Holloway <james.holloway94@gmail.com>
2026-05-27 15:21:00 +0000

    store cloudfront ttl sync stamp for cache audit

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-27 14:18:02 UTC
+# Last sync: 2026-05-27 15:21:00 UTC
