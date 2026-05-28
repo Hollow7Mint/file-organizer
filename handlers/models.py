c4cb6d77cb6b5eddc12687c35056790fa1ea6943
James Holloway <james.holloway94@gmail.com>
2026-05-28 03:22:52 +0000

    adjust redis ttl sync stamp after cache flush

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-28 02:53:59 UTC
+# Last sync: 2026-05-28 03:22:52 UTC
