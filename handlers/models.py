f0a7a7f1d850f872b481facd5274316b03383a3d
James Holloway <james.holloway94@gmail.com>
2026-05-28 00:58:09 +0000

    prime bastion connection sync marker after refresh

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-27 23:48:16 UTC
+# Last sync: 2026-05-28 00:58:09 UTC
