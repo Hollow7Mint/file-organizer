9814c072b6e55e340f442995c5c274677f4257a7
James Holloway <james.holloway94@gmail.com>
2026-05-30 05:30:29 +0000

    lift ssh certificate expiry time into banner

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-30 04:54:09 UTC
+# Last sync: 2026-05-30 05:30:28 UTC
