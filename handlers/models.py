9dda1232b0283792562f46e30f7a6225a138d090
James Holloway <james.holloway94@gmail.com>
2026-06-02 01:40:39 +0000

    flush ec2 fleet inventory marker after scan

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-02 00:38:18 UTC
+# Last sync: 2026-06-02 01:40:39 UTC
