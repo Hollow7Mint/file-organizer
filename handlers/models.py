4218832d4fdd75b3b45e454ec63b8a3569e30ece
James Holloway <james.holloway94@gmail.com>
2026-06-04 16:14:56 +0000

    normalize mysql charset sync time in metadata

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-04 15:23:21 UTC
+# Last sync: 2026-06-04 16:14:56 UTC
