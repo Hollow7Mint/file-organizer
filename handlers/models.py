d00a739714fe16cbef7202fd8982d2927e81dff1
James Holloway <james.holloway94@gmail.com>
2026-06-04 21:54:52 +0000

    anchor mysql parameter sync after maintenance window

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-04 20:37:08 UTC
+# Last sync: 2026-06-04 21:54:52 UTC
