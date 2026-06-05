62019a1ff28c30e8606bef24155b72d562932985
James Holloway <james.holloway94@gmail.com>
2026-06-05 22:32:48 +0000

    transfer ssh config time to bastion banner

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-05 21:29:00 UTC
+# Last sync: 2026-06-05 22:32:48 UTC
