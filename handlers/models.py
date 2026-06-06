3e0745b274be532b1073f97d49c127562e3a3959
James Holloway <james.holloway94@gmail.com>
2026-06-06 12:19:27 +0000

    push mysql connection pool sync after refresh

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-06 11:36:56 UTC
+# Last sync: 2026-06-06 12:19:27 UTC
