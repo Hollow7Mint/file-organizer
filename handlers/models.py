54eda04146ca3830b22d08497e8adc0ce4a2fa0e
James Holloway <james.holloway94@gmail.com>
2026-06-06 09:08:18 +0000

    set ssh tunnel sync stamp in config

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-06 07:22:32 UTC
+# Last sync: 2026-06-06 09:08:18 UTC
