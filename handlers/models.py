8a2466a40d2f7d21a63f94d91857f1249257c1e1
James Holloway <james.holloway94@gmail.com>
2026-06-07 00:50:45 +0000

    unify ec2 placement sync with metadata heartbeat

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-06 23:45:17 UTC
+# Last sync: 2026-06-07 00:50:44 UTC
