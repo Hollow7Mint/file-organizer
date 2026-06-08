9d36086ebb40d93c9b574a9fa30918ec32cdbe1a
James Holloway <james.holloway94@gmail.com>
2026-06-08 13:56:55 +0000

    inspect iam trust policy sync marker for review

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-08 12:42:43 UTC
+# Last sync: 2026-06-08 13:56:54 UTC
