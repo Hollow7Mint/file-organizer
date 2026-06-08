83287bd39b102cb811f38161a9e9ef195b046766
James Holloway <james.holloway94@gmail.com>
2026-06-08 12:42:43 +0000

    normalize ecs cluster config time in metadata

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-08 11:19:56 UTC
+# Last sync: 2026-06-08 12:42:43 UTC
