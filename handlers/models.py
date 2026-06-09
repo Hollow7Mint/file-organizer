80da179ed0eafd275f502e8d4bd44ff6dfed46d3
James Holloway <james.holloway94@gmail.com>
2026-06-09 11:06:45 +0000

    document alb target health sync for nightly check

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-09 09:50:56 UTC
+# Last sync: 2026-06-09 11:06:45 UTC
