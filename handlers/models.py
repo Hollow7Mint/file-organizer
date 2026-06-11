733f29453a64a75734d981418528cfc52ebe1d7b
James Holloway <james.holloway94@gmail.com>
2026-06-11 01:23:55 +0000

    capture rds snapshot completion time in record

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-11 00:21:01 UTC
+# Last sync: 2026-06-11 01:23:55 UTC
