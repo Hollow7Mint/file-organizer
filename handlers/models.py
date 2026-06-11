f8e1c868e148c6904c8996c7ada31378ff71674a
James Holloway <james.holloway94@gmail.com>
2026-06-11 03:29:41 +0000

    bridge postgres foreign key sync through config

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-11 02:52:37 UTC
+# Last sync: 2026-06-11 03:29:41 UTC
