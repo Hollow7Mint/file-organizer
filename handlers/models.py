ef8588765e7344b295ee60cabd8937f0dfc8d5ec
James Holloway <james.holloway94@gmail.com>
2026-06-11 21:51:19 +0000

    post postgres replication heartbeat to config

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-11 20:47:45 UTC
+# Last sync: 2026-06-11 21:51:19 UTC
