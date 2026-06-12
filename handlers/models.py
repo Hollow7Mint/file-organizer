da9fb044bbcf6861e0f0beb83c350f9658ac8584
James Holloway <james.holloway94@gmail.com>
2026-06-12 19:26:08 +0000

    advance ssh key trust sync into config record

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-12 18:52:09 UTC
+# Last sync: 2026-06-12 19:26:08 UTC
