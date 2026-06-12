a8dd4357c46734a2db18e2b88233bc00a8cc2977
James Holloway <james.holloway94@gmail.com>
2026-06-12 20:15:22 +0000

    resync parameter store path time for nightly audit

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-12 19:26:08 UTC
+# Last sync: 2026-06-12 20:15:22 UTC
