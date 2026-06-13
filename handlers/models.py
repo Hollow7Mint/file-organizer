7322ff4719b386a7c00a666da2d2a2338fd087cd
James Holloway <james.holloway94@gmail.com>
2026-06-13 04:29:44 +0000

    inject known_hosts sync marker into config

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-13 03:38:29 UTC
+# Last sync: 2026-06-13 04:29:44 UTC
