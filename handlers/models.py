a00f21c8239b6cdb5ab21ff1b605d6d746e320c0
James Holloway <james.holloway94@gmail.com>
2026-06-15 16:16:30 +0000

    carry mongodb oplog sync stamp to config

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-15 15:33:18 UTC
+# Last sync: 2026-06-15 16:16:30 UTC
