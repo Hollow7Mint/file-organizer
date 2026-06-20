beec0a1d91be2fab256bd58216f47ef3d010feb5
James Holloway <james.holloway94@gmail.com>
2026-06-20 19:36:47 +0000

    advance mysql gtid sync into config record

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-20 18:32:48 UTC
+# Last sync: 2026-06-20 19:36:47 UTC
