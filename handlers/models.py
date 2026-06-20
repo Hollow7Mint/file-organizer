65e53e61f644bcdb0db683a257dfdf0e8c57ebed
James Holloway <james.holloway94@gmail.com>
2026-06-20 10:32:50 +0000

    annotate ec2 ami bake time for deploy record

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-20 09:53:54 UTC
+# Last sync: 2026-06-20 10:32:50 UTC
