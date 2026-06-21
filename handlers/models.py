51eec3c59628cc2379b411baa7b95e9446e57fe7
James Holloway <james.holloway94@gmail.com>
2026-06-21 14:34:07 +0000

    catalog bastion session count sync after audit

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-21 13:46:06 UTC
+# Last sync: 2026-06-21 14:34:07 UTC
