8f129228a5abeaf733ecfc51997f14d24848ef31
James Holloway <james.holloway94@gmail.com>
2026-06-21 13:46:06 +0000

    mirror guardduty archive time in metadata record

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-21 13:05:21 UTC
+# Last sync: 2026-06-21 13:46:06 UTC
