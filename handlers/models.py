6c253fda73676ec03cdac686283126b84ed1ea0b
James Holloway <james.holloway94@gmail.com>
2026-06-23 09:31:17 +0000

    restore ecr scan result time for image audit

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-23 08:51:51 UTC
+# Last sync: 2026-06-23 09:31:17 UTC
