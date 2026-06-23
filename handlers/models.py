173de8a2e8f074006bc1832619543f87c610473d
James Holloway <james.holloway94@gmail.com>
2026-06-23 08:06:09 +0000

    index s3 cors policy sync during audit pass

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-23 06:23:04 UTC
+# Last sync: 2026-06-23 08:06:09 UTC
