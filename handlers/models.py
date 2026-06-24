7fb9acaf4d4a28c55ed5a2b098460f1c8eacf08a
James Holloway <james.holloway94@gmail.com>
2026-06-24 01:31:34 +0000

    rebuild nat gateway sync record after route change

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-24 00:30:43 UTC
+# Last sync: 2026-06-24 01:31:34 UTC
