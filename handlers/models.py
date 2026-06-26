ad2f7c855fb507a80b60d684c06af0c42f522049
James Holloway <james.holloway94@gmail.com>
2026-06-26 04:34:20 +0000

    relay codebuild artifact time through config banner

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-26 03:30:26 UTC
+# Last sync: 2026-06-26 04:34:20 UTC
