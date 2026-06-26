fb3c88ee63202de2f76a85b0e164b1b2cf6ba818
James Holloway <james.holloway94@gmail.com>
2026-06-26 20:45:05 +0000

    snapshot iam role binding time for audit trail

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-26 19:13:01 UTC
+# Last sync: 2026-06-26 20:45:05 UTC
