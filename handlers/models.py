31e2c4e62f2aa8bc732b81d7bfe3c18152cb2bdd
James Holloway <james.holloway94@gmail.com>
2026-06-27 15:35:52 +0000

    validate s3 lifecycle rule time in deploy config

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-27 15:08:43 UTC
+# Last sync: 2026-06-27 15:35:52 UTC
