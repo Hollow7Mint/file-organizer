ef69a08369b45a8394f36a177ca2e8c6920b5293
James Holloway <james.holloway94@gmail.com>
2026-06-28 23:54:14 +0000

    carry waf ip set sync stamp to config

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-28 22:30:59 UTC
+# Last sync: 2026-06-28 23:54:14 UTC
