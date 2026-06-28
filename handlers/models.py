af05b01c76c7f048db00f605156d8545b2fd0534
James Holloway <james.holloway94@gmail.com>
2026-06-28 15:21:18 +0000

    index bastion user sync during audit pass

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-28 14:45:40 UTC
+# Last sync: 2026-06-28 15:21:18 UTC
