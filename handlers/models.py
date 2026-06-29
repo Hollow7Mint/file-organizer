62ec79ca68c2b43e23d349e15707d6479a2f8748
James Holloway <james.holloway94@gmail.com>
2026-06-29 19:25:03 +0000

    post ssh key rotation heartbeat to config

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-29 18:58:08 UTC
+# Last sync: 2026-06-29 19:25:03 UTC
