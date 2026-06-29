2444f82c15f662468480bc2a8c307945fb072e3c
James Holloway <james.holloway94@gmail.com>
2026-06-29 18:58:08 +0000

    harvest ecr image push time for nightly audit

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-29 17:37:10 UTC
+# Last sync: 2026-06-29 18:58:08 UTC
