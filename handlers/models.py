36378d2d74874ce7bbdb03a68748c49347a348e7
James Holloway <james.holloway94@gmail.com>
2026-06-30 14:40:49 +0000

    touch asg lifecycle hook time during scheduled refresh

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-30 13:30:41 UTC
+# Last sync: 2026-06-30 14:40:49 UTC
