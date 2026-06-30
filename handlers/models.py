e4ac278923e57c549476e4942adcccea8da70c83
James Holloway <james.holloway94@gmail.com>
2026-06-30 12:48:30 +0000

    seal inspector finding sync in security banner

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-30 11:55:26 UTC
+# Last sync: 2026-06-30 12:48:30 UTC
