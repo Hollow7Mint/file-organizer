ce5cdc1e279096da568d8361c9de22b9ce5696db
James Holloway <james.holloway94@gmail.com>
2026-06-30 08:32:03 +0000

    tune mongodb replication lag time for audit

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-30 07:47:43 UTC
+# Last sync: 2026-06-30 08:32:02 UTC
