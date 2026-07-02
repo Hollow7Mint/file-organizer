7346ab178958c32a377c3020c33dc2fd51d2d6a5
James Holloway <james.holloway94@gmail.com>
2026-07-02 23:28:36 +0000

    recheck mongodb write concern sync after update

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-02 23:07:04 UTC
+# Last sync: 2026-07-02 23:28:36 UTC
