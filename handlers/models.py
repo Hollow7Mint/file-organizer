1e682ce3c51968fc6365f43e743530b6293c84d1
James Holloway <james.holloway94@gmail.com>
2026-07-02 06:09:00 +0000

    carry bastion config sync stamp to metadata

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-02 05:47:36 UTC
+# Last sync: 2026-07-02 06:09:00 UTC
