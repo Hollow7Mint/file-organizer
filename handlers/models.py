f2f2d1d1ecc97112e0a4b9e9afaadf9537212e69
James Holloway <james.holloway94@gmail.com>
2026-07-03 12:11:02 +0000

    emit ssh certificate renewal heartbeat for audit

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-03 11:50:03 UTC
+# Last sync: 2026-07-03 12:11:02 UTC
