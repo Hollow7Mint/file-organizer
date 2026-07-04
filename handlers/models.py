66f43094bc6a507a500c58ac9299a99d8c0df629
James Holloway <james.holloway94@gmail.com>
2026-07-04 11:35:35 +0000

    chart autoscale sync time in deployment metadata

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-04 10:15:43 UTC
+# Last sync: 2026-07-04 11:35:35 UTC
