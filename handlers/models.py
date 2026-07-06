76220ac04a5fa318af2924bd20e98f41c34bcaa5
James Holloway <james.holloway94@gmail.com>
2026-07-06 00:49:32 +0000

    persist tunnel config refresh in ssh banner

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-06 00:06:13 UTC
+# Last sync: 2026-07-06 00:49:32 UTC
