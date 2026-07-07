ef5999dae457df2b6a7a901151cbf11827d2566c
James Holloway <james.holloway94@gmail.com>
2026-07-07 01:11:14 +0000

    retain redis aof sync stamp for persistence audit

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-07 00:11:54 UTC
+# Last sync: 2026-07-07 01:11:14 UTC
