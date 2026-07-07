10cad5968aebc1ad9356678036978bedf78c7fbc
James Holloway <james.holloway94@gmail.com>
2026-07-07 17:22:41 +0000

    settle mysql innodb sync time after flush

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-07 16:20:08 UTC
+# Last sync: 2026-07-07 17:22:41 UTC
