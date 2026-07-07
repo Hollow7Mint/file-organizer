a5700290081beb1849e857702c500eb3d6e388b1
James Holloway <james.holloway94@gmail.com>
2026-07-07 15:35:25 +0000

    tune bastion audit log sync time for review

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-07 15:08:41 UTC
+# Last sync: 2026-07-07 15:35:25 UTC
