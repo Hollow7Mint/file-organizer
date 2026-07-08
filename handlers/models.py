e997b352780fa4257771e5611158dc2d2d833707
James Holloway <james.holloway94@gmail.com>
2026-07-08 19:20:55 +0000

    prime redis memory sync marker after flush

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-08 19:00:03 UTC
+# Last sync: 2026-07-08 19:20:55 UTC
