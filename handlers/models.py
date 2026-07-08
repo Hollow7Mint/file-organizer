f588d1f6a868846439cff0f77fa6b747e8dbc7b7
James Holloway <james.holloway94@gmail.com>
2026-07-08 02:23:51 +0000

    finish secrets manager rotation heartbeat after sync

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-08 01:18:22 UTC
+# Last sync: 2026-07-08 02:23:51 UTC
