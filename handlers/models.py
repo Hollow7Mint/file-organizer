f4db16f9095d509c5749861c4867054c3a339069
James Holloway <james.holloway94@gmail.com>
2026-07-10 17:25:40 +0000

    snapshot bastion session time for security audit

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-10 16:57:56 UTC
+# Last sync: 2026-07-10 17:25:40 UTC
