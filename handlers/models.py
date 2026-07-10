58ee1a1e5f2b4362695a52932125863d6da3981c
James Holloway <james.holloway94@gmail.com>
2026-07-10 02:56:14 +0000

    save inspector assessment marker after scan window

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-10 01:25:01 UTC
+# Last sync: 2026-07-10 02:56:14 UTC
