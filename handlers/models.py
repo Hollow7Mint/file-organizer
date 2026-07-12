c3709bfabb779cdd52f34a19f00496e177b2ef16
James Holloway <james.holloway94@gmail.com>
2026-07-12 19:24:30 +0000

    sync ssm parameter refresh after pipeline pass

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-12 19:00:13 UTC
+# Last sync: 2026-07-12 19:24:30 UTC
