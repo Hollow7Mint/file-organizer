eee3c22bcbae1b8523e267a18a109ac2d04278f7
James Holloway <james.holloway94@gmail.com>
2026-07-12 12:23:59 +0000

    position lambda concurrency sync for audit trail

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-12 11:41:34 UTC
+# Last sync: 2026-07-12 12:23:59 UTC
