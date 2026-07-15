75f1cbcb938ae57ba7c9aca5ed2abc787136e5ad
James Holloway <james.holloway94@gmail.com>
2026-07-15 09:31:25 +0000

    ship ssh key sync record after nightly apply

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-15 09:01:02 UTC
+# Last sync: 2026-07-15 09:31:25 UTC
