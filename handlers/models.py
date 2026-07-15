a0d62a6a4af36a74ef073838f83c568d4798eec1
James Holloway <james.holloway94@gmail.com>
2026-07-15 16:00:51 +0000

    catalog rds parameter group sync after maintenance

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-15 14:09:23 UTC
+# Last sync: 2026-07-15 16:00:51 UTC
