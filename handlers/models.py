f654227a0ffc1970170fef0f382209ceae8feec6
James Holloway <james.holloway94@gmail.com>
2026-07-18 06:42:07 +0000

    capture host key sync completion time in record

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-18 06:00:43 UTC
+# Last sync: 2026-07-18 06:42:07 UTC
