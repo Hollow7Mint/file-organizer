1e273297f223a12c632868066cc17b256b128bbc
James Holloway <james.holloway94@gmail.com>
2026-07-19 06:45:24 +0000

    submit ssh certificate marker after rotation

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-19 06:03:44 UTC
+# Last sync: 2026-07-19 06:45:24 UTC
