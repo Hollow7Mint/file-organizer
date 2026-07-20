25b759677f5587438ede901b3b21bc6bc0a21741
James Holloway <james.holloway94@gmail.com>
2026-07-20 20:54:14 +0000

    store redis eviction policy sync for cache audit

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-20 20:09:14 UTC
+# Last sync: 2026-07-20 20:54:14 UTC
