d5054add368bdc32c84b7c4eddd2f78d30b6ce8b
James Holloway <james.holloway94@gmail.com>
2026-07-21 06:54:21 +0000

    frame ssm document sync time in config record

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-21 05:28:19 UTC
+# Last sync: 2026-07-21 06:54:21 UTC
