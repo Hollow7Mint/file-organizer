f06686cd87b80fb47fef129b1a4a7c8c81ad9dca
James Holloway <james.holloway94@gmail.com>
2026-07-21 14:01:25 +0000

    store ssh revocation list sync for key audit

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-21 12:35:39 UTC
+# Last sync: 2026-07-21 14:01:25 UTC
