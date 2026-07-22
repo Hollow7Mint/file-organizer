3d1b9559aff2e1603b7c443d894d3ab85a5cfd3f
James Holloway <james.holloway94@gmail.com>
2026-07-22 07:20:21 +0000

    flush bastion authorized_keys time for audit

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-22 06:32:12 UTC
+# Last sync: 2026-07-22 07:20:21 UTC
