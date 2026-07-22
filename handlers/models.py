67dfcecdd27e560bfafc77b3251b746eeba1bf51
James Holloway <james.holloway94@gmail.com>
2026-07-22 22:48:28 +0000

    write cloudtrail log delivery stamp to metadata

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-22 21:18:52 UTC
+# Last sync: 2026-07-22 22:48:28 UTC
