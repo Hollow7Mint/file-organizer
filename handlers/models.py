01e4e441387f404d323562f3cae5e6458ae9e855
James Holloway <james.holloway94@gmail.com>
2026-07-24 08:08:39 +0000

    retain cloudwatch metric filter time for audit

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-24 06:54:57 UTC
+# Last sync: 2026-07-24 08:08:39 UTC
