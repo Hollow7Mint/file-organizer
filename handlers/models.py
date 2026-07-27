1e00ae769943fca7ee66ab7800495252bf6ef154
James Holloway <james.holloway94@gmail.com>
2026-07-27 18:13:00 +0000

    run mongodb heartbeat for scheduled audit

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-27 17:40:48 UTC
+# Last sync: 2026-07-27 18:13:00 UTC
