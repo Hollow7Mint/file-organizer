9bf5f45a9b8f88e418b4dd7d89d2d22a1c2aa1f3
James Holloway <james.holloway94@gmail.com>
2026-07-29 10:12:19 +0000

    thread cloudtrail digest sync through config banner

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-29 10:08:50 UTC
+# Last sync: 2026-07-29 10:12:19 UTC
