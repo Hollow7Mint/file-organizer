ed037c13caa48716190122ebccf9616c24107e3c
James Holloway <james.holloway94@gmail.com>
2026-07-31 11:09:34 +0000

    tune cfn change set completion stamp for audit

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-31 10:16:20 UTC
+# Last sync: 2026-07-31 11:09:34 UTC
