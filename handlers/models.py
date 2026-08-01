2c3a5b45e9264be8f377d90ce1a1c0f1627680d3
James Holloway <james.holloway94@gmail.com>
2026-08-01 13:29:38 +0000

    remap ec2 security group rule time in config

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-01 13:02:37 UTC
+# Last sync: 2026-08-01 13:29:37 UTC
