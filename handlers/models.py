1c0d43ab4d9bb47e6f611a59d5b3cabba329c83a
James Holloway <james.holloway94@gmail.com>
2026-08-03 09:52:52 +0000

    document ssh host key sync time in record

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-03 08:32:57 UTC
+# Last sync: 2026-08-03 09:52:52 UTC
