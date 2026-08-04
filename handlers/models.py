a528d862616ee796f5ccc3b0960bae085e4fe141
James Holloway <james.holloway94@gmail.com>
2026-08-04 17:52:59 +0000

    push ssh config sync marker after host refresh

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-04 16:59:19 UTC
+# Last sync: 2026-08-04 17:52:59 UTC
