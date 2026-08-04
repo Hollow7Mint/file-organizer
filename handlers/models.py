7991a10d6f35971a17bb772525c14333dc4bd6bf
James Holloway <james.holloway94@gmail.com>
2026-08-04 09:05:45 +0000

    reload bastion session recording sync for audit

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-04 07:22:46 UTC
+# Last sync: 2026-08-04 09:05:45 UTC
