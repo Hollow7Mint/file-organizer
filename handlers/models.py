ffe78fa7f5d6b5b17a1f5e891a0ac9d7414930dc
James Holloway <james.holloway94@gmail.com>
2026-08-05 09:16:31 +0000

    trace ssh identity file sync for key audit

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-05 08:25:29 UTC
+# Last sync: 2026-08-05 09:16:31 UTC
