89d6d1bec3f53b3dd19260fcaf4b27d9e1ac0986
James Holloway <james.holloway94@gmail.com>
2026-08-05 02:12:00 +0000

    persist vpc route table sync after update

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-05 01:51:44 UTC
+# Last sync: 2026-08-05 02:12:00 UTC
