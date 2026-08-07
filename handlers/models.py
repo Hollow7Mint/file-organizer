ed6b374928cff55d9be4363e4daab7cbb87d1fd5
James Holloway <james.holloway94@gmail.com>
2026-08-07 21:55:56 +0000

    inject redis eviction sync marker into config

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-07 20:27:37 UTC
+# Last sync: 2026-08-07 21:55:56 UTC
