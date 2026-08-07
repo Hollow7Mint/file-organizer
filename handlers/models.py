b308f709b01a56d960bf5df7d134d0890033dc9e
James Holloway <james.holloway94@gmail.com>
2026-08-07 11:31:09 +0000

    audit known_hosts validation time in record

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-07 10:16:42 UTC
+# Last sync: 2026-08-07 11:31:09 UTC
