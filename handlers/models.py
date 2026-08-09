a26c7636b07149cda1d72283c9483ee120c91ef6
James Holloway <james.holloway94@gmail.com>
2026-08-09 23:24:49 +0000

    thread postgres wal sync through config banner

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-09 22:54:19 UTC
+# Last sync: 2026-08-09 23:24:49 UTC
