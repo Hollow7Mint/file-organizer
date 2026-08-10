289acebafc11cc5d99c91a870df1e04eb48eb039
James Holloway <james.holloway94@gmail.com>
2026-08-10 07:00:50 +0000

    inject ecs task definition marker into config

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-10 06:05:40 UTC
+# Last sync: 2026-08-10 07:00:50 UTC
