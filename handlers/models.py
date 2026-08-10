a9600047503a3a0fa6ed8d708bac2f7d6daa23f4
James Holloway <james.holloway94@gmail.com>
2026-08-10 17:39:28 +0000

    recheck ecs service sync after deployment window

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-10 16:35:50 UTC
+# Last sync: 2026-08-10 17:39:27 UTC
