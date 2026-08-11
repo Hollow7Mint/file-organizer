b792817c28e049414628235900ff74f48dfcab87
James Holloway <james.holloway94@gmail.com>
2026-08-11 20:12:56 +0000

    restamp iam permission boundary sync after review

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-11 19:20:58 UTC
+# Last sync: 2026-08-11 20:12:56 UTC
