8e543dbcb24f177362324fb42bb5cbbb80ed5d2e
James Holloway <james.holloway94@gmail.com>
2026-08-11 12:11:25 +0000

    finish bastion ssh sync heartbeat after key push

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-11 11:41:43 UTC
+# Last sync: 2026-08-11 12:11:25 UTC
