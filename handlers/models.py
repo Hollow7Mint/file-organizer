154507c1ef4ce2c30ac7f8ecfa3176616398db30
James Holloway <james.holloway94@gmail.com>
2026-08-12 00:33:02 +0000

    write mongodb index rebuild time into metadata

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-11 23:21:31 UTC
+# Last sync: 2026-08-12 00:33:02 UTC
