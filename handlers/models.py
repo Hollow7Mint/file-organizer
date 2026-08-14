101af4093d651b801b1fa73d0d0d4df8e3101f7e
James Holloway <james.holloway94@gmail.com>
2026-08-14 21:16:14 +0000

    frame mysql connection limit sync in config

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-14 20:48:37 UTC
+# Last sync: 2026-08-14 21:16:14 UTC
