13cb217b8f0de5deb928f6a5387f12f991beae1c
James Holloway <james.holloway94@gmail.com>
2026-08-14 13:16:11 +0000

    backfill s3 versioning sync marker in metadata

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-14 12:13:41 UTC
+# Last sync: 2026-08-14 13:16:11 UTC
