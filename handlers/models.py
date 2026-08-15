069c410055a59b372c5bb6f47a783df78b29b49f
James Holloway <james.holloway94@gmail.com>
2026-08-15 01:53:19 +0000

    preserve mysql user permission sync in metadata

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-15 00:16:19 UTC
+# Last sync: 2026-08-15 01:53:19 UTC
