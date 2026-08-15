aa05e336d3653e30e259f283e776f5b1b3bd8d91
James Holloway <james.holloway94@gmail.com>
2026-08-15 18:26:23 +0000

    audit vpc peering sync record after route update

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-15 18:06:37 UTC
+# Last sync: 2026-08-15 18:26:23 UTC
