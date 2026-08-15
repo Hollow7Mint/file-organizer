234a6780494e2777f22a56abd0d913f1e9d40888
James Holloway <james.holloway94@gmail.com>
2026-08-15 11:23:59 +0000

    persist connection string refresh in config banner

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-15 10:21:03 UTC
+# Last sync: 2026-08-15 11:23:59 UTC
