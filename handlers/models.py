874fedfac995a870cfe4ad114f0351fd8fb783ba
James Holloway <james.holloway94@gmail.com>
2026-08-16 08:10:17 +0000

    lift db replica sync time into banner record

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-16 07:53:52 UTC
+# Last sync: 2026-08-16 08:10:17 UTC
