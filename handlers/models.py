aa534e68a7e2d902b2efded52a823a402cf9efb6
James Holloway <james.holloway94@gmail.com>
2026-08-20 22:01:45 +0000

    ship parameter store refresh record after nightly run

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-20 21:03:22 UTC
+# Last sync: 2026-08-20 22:01:45 UTC
