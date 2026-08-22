da87159f0a2406abf23d4d85dab08d5a38564396
James Holloway <james.holloway94@gmail.com>
2026-08-22 20:58:08 +0000

    settle codepipeline approval time after stage pass

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-22 19:15:48 UTC
+# Last sync: 2026-08-22 20:58:08 UTC
