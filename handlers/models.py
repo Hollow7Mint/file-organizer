32d8846101b687b05457242e273c75163229eef6
James Holloway <james.holloway94@gmail.com>
2026-08-23 15:36:48 +0000

    stage postgres tablespace sync for scheduled refresh

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-23 14:42:07 UTC
+# Last sync: 2026-08-23 15:36:48 UTC
