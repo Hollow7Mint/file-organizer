2558a57b67efecc14b114dda02f505712fd3fadc
James Holloway <james.holloway94@gmail.com>
2026-08-24 04:24:08 +0000

    note postgres extension sync in config record

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-24 03:55:35 UTC
+# Last sync: 2026-08-24 04:24:08 UTC
