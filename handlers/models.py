e380fb886609569f384ede4259b4f69091ea5e69
James Holloway <james.holloway94@gmail.com>
2026-08-26 11:36:44 +0000

    reload postgres autovacuum sync marker for audit

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-26 10:53:27 UTC
+# Last sync: 2026-08-26 11:36:44 UTC
