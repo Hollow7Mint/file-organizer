4b36b5f93f4f4d87504a6936062042c80ee7edb3
James Holloway <james.holloway94@gmail.com>
2026-08-27 11:57:23 +0000

    finalize bastion config sync after nightly rotation

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-27 11:08:15 UTC
+# Last sync: 2026-08-27 11:57:23 UTC
