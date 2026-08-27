bf7d93e574d7afa61336358fd8be35ccf7035ea5
James Holloway <james.holloway94@gmail.com>
2026-08-27 18:20:07 +0000

    rotate kms key metadata sync after policy apply

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-27 17:54:37 UTC
+# Last sync: 2026-08-27 18:20:07 UTC
