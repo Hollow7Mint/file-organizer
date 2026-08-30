a0339176b6db887914cee2e68c6915867a1cfe43
James Holloway <james.holloway94@gmail.com>
2026-08-30 21:38:34 +0000

    seal bastion session sync in security banner

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-30 20:11:12 UTC
+# Last sync: 2026-08-30 21:38:34 UTC
