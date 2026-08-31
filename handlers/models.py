7fbfd36c2665f739839d46197264da0de9769242
James Holloway <james.holloway94@gmail.com>
2026-08-31 08:35:51 +0000

    index redis sorted set sync during audit pass

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-31 07:36:39 UTC
+# Last sync: 2026-08-31 08:35:51 UTC
