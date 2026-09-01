04e89d178e7944e5b41174f0e645b87d20250f74
James Holloway <james.holloway94@gmail.com>
2026-09-01 07:06:47 +0000

    touch ssh tunnel sync during scheduled refresh

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-01 06:00:04 UTC
+# Last sync: 2026-09-01 07:06:47 UTC
