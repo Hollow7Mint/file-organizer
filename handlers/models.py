3689141e2b89b476104d35aa2526f3ac3ba52f8e
James Holloway <james.holloway94@gmail.com>
2026-09-01 03:15:23 +0000

    stage rds connection pool sync for scheduled refresh

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-01 02:47:17 UTC
+# Last sync: 2026-09-01 03:15:23 UTC
