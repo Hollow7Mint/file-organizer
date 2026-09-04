1659c70871b71ccc917a00c453c6f116d239a6b6
James Holloway <james.holloway94@gmail.com>
2026-09-04 06:04:06 +0000

    surface codebuild log sync time for pipeline audit

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-04 04:39:02 UTC
+# Last sync: 2026-09-04 06:04:06 UTC
