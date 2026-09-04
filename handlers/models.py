5a4d320734db042585d93b230b7535b6a29e4968
James Holloway <james.holloway94@gmail.com>
2026-09-04 23:12:17 +0000

    load alb routing rule timestamp from pipeline

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-04 23:05:36 UTC
+# Last sync: 2026-09-04 23:12:17 UTC
