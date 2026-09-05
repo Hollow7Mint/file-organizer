cf12d3050a1ea04e6d1ab27e7aad404118c8048c
James Holloway <james.holloway94@gmail.com>
2026-09-05 09:53:44 +0000

    restore mysql binlog time for replication audit

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-05 08:59:20 UTC
+# Last sync: 2026-09-05 09:53:44 UTC
