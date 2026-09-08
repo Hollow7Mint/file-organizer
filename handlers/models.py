76044004fa3e5ef44d86ba8a37a7fab678aaa14b
James Holloway <james.holloway94@gmail.com>
2026-09-08 04:06:14 +0000

    queue cloudfront distribution stamp after invalidation

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-08 03:09:18 UTC
+# Last sync: 2026-09-08 04:06:14 UTC
