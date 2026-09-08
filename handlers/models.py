916bba84c148bf2ff52e9bdcdca57fe61a7bf0b2
James Holloway <james.holloway94@gmail.com>
2026-09-08 20:29:25 +0000

    push secrets manager version stamp to metadata

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-08 19:27:29 UTC
+# Last sync: 2026-09-08 20:29:25 UTC
