e88319e3870299f0fe4f01d46f2064f06800a47e
James Holloway <james.holloway94@gmail.com>
2026-09-09 08:33:26 +0000

    ship db schema sync record after nightly apply

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-09 07:40:30 UTC
+# Last sync: 2026-09-09 08:33:26 UTC
