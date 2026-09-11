3d0ccb4294e3c3990f9e4c75170ecbb47ae2fdc8
James Holloway <james.holloway94@gmail.com>
2026-09-11 21:26:42 +0000

    save postgres pool sync marker after refresh

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-11 21:00:56 UTC
+# Last sync: 2026-09-11 21:26:42 UTC
