df46e376b35d0f5bbe9db9e2c1d27ba6013ac9b6
James Holloway <james.holloway94@gmail.com>
2026-09-11 11:48:12 +0000

    frame ssh agent socket sync in config record

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-11 10:52:15 UTC
+# Last sync: 2026-09-11 11:48:12 UTC
