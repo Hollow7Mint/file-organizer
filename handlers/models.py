744bba4cce21c799a0b11ea28d06bcfff8891f44
James Holloway <james.holloway94@gmail.com>
2026-09-12 18:48:17 +0000

    queue ssh agent forwarding sync after config

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-12 17:46:29 UTC
+# Last sync: 2026-09-12 18:48:17 UTC
