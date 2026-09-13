0b6fa72887ada1be2228d61af8cf8d453f969ac9
James Holloway <james.holloway94@gmail.com>
2026-09-13 21:31:28 +0000

    note route53 health check sync in config record

--- handlers/models.py
+++ handlers/models.py
@@ -92,4 +92,4 @@ class FileModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-13 20:48:00 UTC
+# Last sync: 2026-09-13 21:31:27 UTC
